13715eaf4c65170b10eb046c45b795fcac473926
fankunet-arch <fankunet@gmail.com>
2025-12-18 01:28:35 +0100

    Add files via upload

--- app/vis/config_vis/env_vis.php
+++ app/vis/config_vis/env_vis.php
@@ -33,13 +33,13 @@ define('VIS_R2_SECRET_ACCESS_KEY', 'tybrN0C7CxUA81cafLlH5382mzTea96V');
 define('VIS_R2_BUCKET_NAME', 'vis-videos'); 
 
 // 4. QNAP S3 服务地址 (后台上传用)
-define('VIS_R2_ENDPOINT', 'https://192.168.1.128:8010'); 
+define('VIS_R2_ENDPOINT', 'http://192.168.1.128:8010'); 
 
 // 5. 区域 (默认)
 define('VIS_R2_REGION', 'us-east-1');
 
 // 6. 公共访问域名 (前台显示用)
-define('VIS_R2_PUBLIC_URL', 'https://192.168.1.128:8010/v1/AUTH_QuObjects/vis-videos');
+define('VIS_R2_PUBLIC_URL', 'http://192.168.1.128:8010/v1/AUTH_QuObjects/vis-videos');
 
 // 签名URL有效期
 define('VIS_SIGNED_URL_EXPIRES', 300);
